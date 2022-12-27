7b7c6436726f34cce2d38e38c75c76e2306ce11c
ThepsirinN <thepsirin.n@alumni.chula.ac.th>
2022-12-27 08:08:05 +0700

    Chore:fix Open Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { MyPageContext } from "./context/PageContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import AboutMe from "./components/AboutMe";
 import Expreience from "./components/Expreience";
@@ -11,27 +11,44 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
-  const { page,setPageValue } = MyPageContext();
+  const [isLoading, setIsLoading] = useState(true);
+  const { page, setPageValue } = MyPageContext();
 
   useEffect(() => {
-    setPageValue("🧑About")
-    /* return () => {
-    } */
-  }, [])
-  
-  
+    let tid = setTimeout(() => {
+      setPageValue("🧑About");
+      setIsLoading(false);
+    }, 2000);
+    return () => {
+      clearTimeout(tid);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <Nav />
-      <main className="Information">
-        {page === "🧑About" && <AboutMe />}
-        {page === "🤯Experience" && <Expreience />}
-        {page === "📙Portfolio" && <ExamplePort />}
-        {page === "🏫Education" && <Education />}
-        {page === "🏃‍♂️Skill" && <Skill />}
-        {page === "📞Contact" && <Contact />}
-      </main>
-      <Footer />
+      {isLoading && (
+        <div className="loading-section">
+          <div className="loader"></div>
+          <div className="loader-text">
+            <span>Barko.</span>
+            <span className="loader-inner-text">Bank</span>
+          </div>
+        </div>
+      )}
+      {!isLoading && (
+        <>
+          <Nav />
+          <main className="Information">
+            {page === "🧑About" && <AboutMe />}
+            {page === "🤯Experience" && <Expreience />}
+            {page === "📙Portfolio" && <ExamplePort />}
+            {page === "🏫Education" && <Education />}
+            {page === "🏃‍♂️Skill" && <Skill />}
+            {page === "📞Contact" && <Contact />}
+          </main>
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
